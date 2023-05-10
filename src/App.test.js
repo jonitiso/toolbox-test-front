40676964d602d5ab2189cd6d6e09436ad5b2dd92
Jonathan Tovar Diaz <jonitiso@gmail.com>
2023-05-10 13:58:29 -0500

    fix: enable npm test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import {render, screen} from '@testing-library/react';
+import App from './App';
 import AppTable from "./components/AppTable";
 
 test('renders a table with correct columns', () => {
